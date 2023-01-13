95503f130f0294fe4ce27f965963c696f0d1a63e
100larczyk <100larczyk.m@gmail.com>
2023-01-13 18:18:18 +0100

    value inputed by user is held as object. Added logic to clear button

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const addNumber = document.querySelector(".add");
 const subtractNumber = document.querySelector(".subtract");
 const multiplyNumber = document.querySelector(".multiply");
 const divideNumber = document.querySelector(".divide");
-let result;
 
 function add(a, b) {
   return a + b;
@@ -34,11 +33,25 @@ function operate(a, b, c) {
   return;
 }
 //apear clicked numbers on the calculaor display
-number.forEach((number) =>
-  number.addEventListener("click", function (event) {
-    const numberClicked = event.target.value;
-    output.textContent += numberClicked;
-    result = parseInt(output.textContent);
-    console.log(typeof result);
-  })
+let inputedNumber = {};
+//display i number.forEach stwarzają i przechwują wpisaną liczbę do czasu następnej liczby wpisanej przez użytkownika
+function display(event) {
+  const numberClicked = event.target.value;
+  output.textContent += numberClicked; //pokazuje kolejne po sobie klinięte liczby na wyświetlaczu
+  inputedNumber.number = parseInt(output.textContent);
+  console.log(inputedNumber.number);
+}
+number.forEach((number) => number.addEventListener("click", display));
+
+let result = {};
+clear.addEventListener(
+  "click",
+  () => delete inputedNumber.number,
+  delete result.total
 );
+
+/* funkcja, która po kliknięciu w operator: 
+ 1)uzyskuje wartość inputedNumber.number jako argument a;
+ 2)kasuje zawatość wyświetlacza;
+ 3)uzyskuje wartość inputedNumber.number, która jest nową wpisaną przez użytkownika liczbą jako argument c; (muszę zrobić if (kliknięto któryś operator) (użyj inputedNumber jako argument c))
+ 4)wywołuje funkcję operate;*/
