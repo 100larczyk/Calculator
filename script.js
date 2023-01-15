3bda25e6285f3dfe054aeeab4dda41bfcae57e36
100larczyk <100larczyk.m@gmail.com>
2023-01-15 12:15:44 +0100

    added handleclick for operators

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// global varaibles
 const output = document.querySelector(".output");
 const number = document.querySelectorAll(".number");
 const clear = document.querySelector(".clear");
@@ -6,52 +5,80 @@ const addNumber = document.querySelector(".add");
 const subtractNumber = document.querySelector(".subtract");
 const multiplyNumber = document.querySelector(".multiply");
 const divideNumber = document.querySelector(".divide");
+const equal = document.querySelector(".equal");
+let firstNumber = 0;
+let currentOperation;
+let lastNumber;
+let lastOperation = 0;
+const allOperations = [addNumber, subtractNumber, multiplyNumber, divideNumber];
+
+number.forEach((number) => number.addEventListener("click", handleNumberClick));
+
+function handleNumberClick(event) {
+  const numberClicked = event.target.value;
+  output.textContent += numberClicked;
+  console.log({ firstNumber, currentOperation });
+  if (firstNumber && currentOperation) {
+    console.log({ lastNumber, numberClicked });
+    lastNumber = lastNumber
+      ? parseInt(`${lastNumber}${numberClicked}`)
+      : parseInt(numberClicked);
+  } else {
+    firstNumber = parseInt(`${firstNumber}${numberClicked}`);
+  }
+  console.log({ firstNumber });
+  console.log({ lastNumber });
+}
+
+allOperations.forEach((number) =>
+  number.addEventListener("click", handleOperationClick)
+);
+
+function handleOperationClick(event) {
+  if (currentOperation) {
+    firstNumber = operate(firstNumber, currentOperation, lastNumber);
+    lastNumber = undefined;
+    currentOperation = undefined;
+    output.textContent = firstNumber;
+    return;
+  }
+  currentOperation = event.target.value;
+  output.textContent += currentOperation;
+}
+
+function clearAll() {
+  currentOperation = 0;
+  lastOperation = 0;
+  result = 0;
+  output.textContent = "";
+}
+
+clear.addEventListener("click", clearAll);
 
 function add(a, b) {
   return a + b;
 }
+
 function subtract(a, b) {
   return a - b;
 }
+
 function multiply(a, b) {
   return a * b;
 }
+
 function divide(a, b) {
   return a / b;
 }
 
-function operate(a, b, c) {
-  if (b === "+") {
-    console.log(add(a, c));
-  } else if (b === "-") {
-    console.log(subtract(a, c));
-  } else if (b === "*") {
-    console.log(multiply(a, c));
-  } else if (b === "/") {
-    console.log(divide(a, c));
+function operate(a, operator, c) {
+  if (operator === "+") {
+    return add(a, c);
+  } else if (operator === "-") {
+    return subtract(a, c);
+  } else if (operator === "*") {
+    return multiply(a, c);
+  } else if (operator === "/") {
+    return divide(a, c);
   }
-  return;
 }
-//apear clicked numbers on the calculaor display
-let inputedNumber = {};
-//display i number.forEach stwarzają i przechwują wpisaną liczbę do czasu następnej liczby wpisanej przez użytkownika
-function display(event) {
-  const numberClicked = event.target.value;
-  output.textContent += numberClicked; //pokazuje kolejne po sobie klinięte liczby na wyświetlaczu
-  inputedNumber.number = parseInt(output.textContent);
-  console.log(inputedNumber.number);
-}
-number.forEach((number) => number.addEventListener("click", display));
-
-let result = {};
-clear.addEventListener(
-  "click",
-  () => delete inputedNumber.number,
-  delete result.total
-);
-
-/* funkcja, która po kliknięciu w operator: 
- 1)uzyskuje wartość inputedNumber.number jako argument a;
- 2)kasuje zawatość wyświetlacza;
- 3)uzyskuje wartość inputedNumber.number, która jest nową wpisaną przez użytkownika liczbą jako argument c; (muszę zrobić if (kliknięto któryś operator) (użyj inputedNumber jako argument c))
- 4)wywołuje funkcję operate;*/
